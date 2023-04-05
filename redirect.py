e444f2800d49d7120c432ce92119d9c8503765fa
Юлия Горбенкова <Y.Yarashenia@NovaCom.by>
2023-04-05 10:25:44 +0300

    lesson 2(ex.6), redirect

---
+++ redirect.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+first_response = response.history[0]
+second_response = response.history[1]
+third_response = response
+
+print(first_response.url)
+print(second_response.url)
+print(third_response.url)
+
